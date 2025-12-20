74b38813c3f6160ca38c757a0aca64d0c4377928
The-First-King <59452275+The-First-King@users.noreply.github.com>
2025-12-20 18:30:17 -0500

    Modify AutoStart to support foreground service
    Updated the onReceive method to handle foreground service for Android 8.0 and above.

--- app/src/main/java/com/mine/autolight/AutoStart.java
+++ app/src/main/java/com/mine/autolight/AutoStart.java
@@ -3,13 +3,20 @@ package com.mine.autolight;
 import android.content.Context;
 import android.content.Intent;
 import android.content.BroadcastReceiver;
+import android.os.Build;
 
 public class AutoStart extends BroadcastReceiver {
-	@Override
-	public void onReceive(Context context, Intent intent) {
-		if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-			Intent pushIntent = new Intent(context, LightService.class);
-			context.startService(pushIntent);
-		}
-	}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent pushIntent = new Intent(context, LightService.class);
+            
+            // Android 8.0 (API 26) and above requires startForegroundService
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(pushIntent);
+            } else {
+                context.startService(pushIntent);
+            }
+        }
+    }
 }
